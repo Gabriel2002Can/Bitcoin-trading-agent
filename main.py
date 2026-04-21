26e90dfee0df2d68dbfe002eded27cabd730242f
Gabriel2002Can <gabriel2002ca@gmail.com>
2026-04-21 20:54:50 -0300

    selecting properties from api response

--- main.py
+++ main.py
@@ -21,6 +21,14 @@ def get_data():
 
     return response
 
-data = get_data().json()        
+current_data = get_data().json() 
 
-print(data)
+btc = current_data["data"][0]
+
+quote_usd = btc["quote"][0]
+
+current_price = quote_usd["price"]
+percent_1h    = quote_usd["percent_change_1h"]
+
+print(f"Current BTC price: ${current_price:,.2f}")
+print(f"Percent change 1h: {percent_1h:.2f}%")
