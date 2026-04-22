b69c5980cfdebd297d575e9cfb9943f0824fbe82
Gabriel2002Can <gabriel2002ca@gmail.com>
2026-04-22 15:02:51 -0300

    Using yfinance as information source

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
 import requests 
 import os
 from dotenv import load_dotenv
-
-load_dotenv(".env")
+import yfinance as yf
 
 def get_data():
 
-    api_key = os.getenv("API_KEY") 
+    return yf.Ticker("BTC-USD")
 
-    response = requests.get(
-        "https://pro-api.coinmarketcap.com/v3/cryptocurrency/quotes/latest",
-        params={                  
-            "slug": "bitcoin",           
-            "convert": "USD"
-        },
-        headers={
-            "X-CMC_PRO_API_KEY": api_key
-        }
-    )
+BTC = get_data() 
 
-    return response
+print("Bitcoin Open : ", round(BTC.info['open'],0))
+print("        Day     : ", round(BTC.info['dayLow'], 0), "-", round(BTC.info['dayHigh'], 0))
+print("        52 Week : ", round(BTC.info['fiftyTwoWeekLow'], 0), "-", round(BTC.info['fiftyTwoWeekHigh'], 0))
 
-current_data = get_data().json() 
+CurrentPrice = BTC.fast_info['lastPrice']
 
-btc = current_data["data"][0]
+print(CurrentPrice)
 
-quote_usd = btc["quote"][0]
-
-current_price = quote_usd["price"]
-percent_1h    = quote_usd["percent_change_1h"]
-
-print(f"Current BTC price: ${current_price:,.2f}")
-print(f"Percent change 1h: {percent_1h:.2f}%")
+print(BTC.info)
